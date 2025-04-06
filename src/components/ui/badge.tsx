31d545c3f152d8768be996702088f2b6aec8281b
Ayesha0300 <ayeshabashir030@gmail.com>
2025-04-06 10:46:10 +0500

    Second commit

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -32,13 +32,13 @@ function Badge({
   ...props
 }: React.ComponentProps<"span"> &
   VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
-  const Comp = asChild ? Slot : "span"
+  const Comp = (asChild ? Slot : "span") as React.ElementType;
 
   return (
     <Comp
       data-slot="badge"
       className={cn(badgeVariants({ variant }), className)}
-      {...props}
+      {...(props as React.HTMLAttributes<HTMLElement>)}
     />
   )
 }
